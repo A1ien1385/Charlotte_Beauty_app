22e72aee216510ced4d13143556f19aae0de7fd9
A1ien1385 <damian.zawadzki@tlen.pl>
2024-08-26 22:01:02 +0200

    delete PIN

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -4,36 +4,25 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrl: './auth.component.scss'
 })
 export class AuthComponent {
-
+ 
   isLoginMode = true;
-  pinIsValid = false;
-  enteredPin: string = '';
-
-  // Poprawny PIN do walidacji
-  private validPin = '5706';
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(form: NgForm) {
-    
-    console.log(form.value);
-    form.reset();
-    // Reset PIN po wysłaniu formularza
-  }
-
-  checkPin() {
-    // Sprawdzanie poprawności PIN-u
-    this.pinIsValid = this.enteredPin === this.validPin;
-    console.log(`Entered PIN: ${this.enteredPin}, IsValid: ${this.pinIsValid}`);
-  }
-
-  ngOnInit() {
-    // Inicjalizacja, PIN powinien być nieważny przy ładowaniu komponentu
-    this.pinIsValid = false;
+   console.log(form.value);
+   form.reset(); 
   }
 }
+
+// {
+//  "rules": {
+//    ".read": "auth != null",  
+//    ".write": "auth != null",  
+//   }
+//  }
